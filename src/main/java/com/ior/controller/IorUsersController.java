7d50dff31e210a63cf516419f28c3d5fdda2d0c1
sawak <3642123814@qq.com>
2026-05-18 23:40:03 +0800

    实现SMTP发送验证码，以及验证码模板

--- src/main/java/com/ior/controller/IorUsersController.java
+++ src/main/java/com/ior/controller/IorUsersController.java
@@ -2,6 +2,8 @@ package com.ior.controller;
 
 import com.ior.domain.vo.Result;
 import com.ior.service.IorUsersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@Tag(name = "用户接口")
+@Tag(name = "用户管理", description = "包含注册、登录、验证码等接口")
 public class IorUsersController {
 
     @Resource
     private IorUsersService iorUsersService;
 
     @GetMapping("/sendcode")
-    public Result sendCode(@RequestParam String email,@RequestParam String type) {
+    @Operation(summary = "发送邮箱验证码", description = "用于注册、登录或找回密码时的身份验证")
+    public Result sendCode(
+            @Parameter(description = "接收验证码的邮箱地址", required = true, example = "user@example.com") 
+            @RequestParam String email,
+            
+            @Parameter(description = "验证码类型: register-注册, login-登录, reset_password-找回密码，change_email-重置邮箱", required = true, example = "register")
+            @RequestParam String type) {
         return iorUsersService.sendCode(email, type);
     }
 
